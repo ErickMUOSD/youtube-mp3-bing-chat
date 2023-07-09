5a79f2b6c98957f53c0d55b81da756b339625f66
Erick Sanabria <martinoli944@gmail.com>
2023-07-09 12:32:26 -0600

    download list of music

--- main.py
+++ main.py
@@ -6,17 +6,31 @@ from pytube import YouTube
 from alive_progress.styles import showtime
 
 
-def dowload_yt_video(url):
+def read_txt():
+    try:
+        file1 = open('/home/erick/Documents/PersonalProject/youtube-downloable-music/urls_file.txt', 'r')
+        return file1.readlines()
+    except Exception as e:
+        print('someth went wrong' + str(e))
+        return None
+
+def download_music_from_list(list_music):
+    if list_music is None:
+        return
+    for url_song in list_music:
+        print('Downloading song: ' + url_song)
+        dowload_yt_video(url_song, destination='/home/erick/Documents/PersonalProject/Music_from_list')
+def dowload_yt_video(url, destination):
     try:
         yt = YouTube(str(url))
         video = yt.streams.filter(only_audio=True).first()
-        destination = '/home/erick/Documents/PersonalProject/Music'
         out_file = video.download(output_path=destination)
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file, new_file)
         print(yt.title + " has been successfully downloaded.")
     except Exception as e:
+        print('song '+ str(url))
         print('something went wrong.' + str(e))
 
 
@@ -40,21 +54,32 @@ async def main():
         text = input()
         print('Enter author:')
         author = input()
+        print('Skip Y/N')
+        choice = input()
+        if choice == 'N' or choice == 'n':
+            prompt = f"Busca un video de  youtube llamado: {text} de {author} y devuelve la url "
+        else:
+            print('Give me a prompt:')
+            prompt = input()
+
         try:
-           response = await bot.ask(prompt=f"Busca un video de youtube llamado: {text} de  {author} y retorna  la url ",
-                             conversation_style=ConversationStyle.precise, simplify_response=True)
+            print(prompt)
+            response = await bot.ask(prompt,
+                                     conversation_style=ConversationStyle.precise, simplify_response=True)
         except Exception as e:
             print(str(e))
             continue
         print(response['text'])
-        url = extract_youtube_url(response['text']  or '')
+        url = extract_youtube_url(response['text'] or '')
         if not url:
             print("No se encontro la url")
         else:
-            dowload_yt_video(url)
+            dowload_yt_video(url, destination= '/home/erick/Documents/PersonalProject/Music')
         print('Leave system ? Y/N')
         end_system = input()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    # list_music = read_txt()
+    # download_music_from_list(list_music)
